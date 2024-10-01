5794c60e11dacce13addf937a0c4242022297fed
Gagik-arch <gagik.chilingaryan2020@gmail.com>
2024-10-01 18:56:25 +0400

    rendering days and weekdays

--- example/main.js
+++ example/main.js
@@ -2,4 +2,21 @@ import Calendar from '../dist/index.js';
 
 const calendar = new Calendar();
 
+const weekContainer = document.getElementById('datewise_week_container');
+const dayContainer = document.getElementById('datewise_day_container');
+
 console.log(calendar);
+
+calendar.days.forEach((day) => {
+    const div = document.createElement('div');
+    dayContainer.appendChild(div);
+    div.classList.add('datewise-day');
+    div.innerHTML = day.label;
+});
+
+calendar.weekDays.forEach((week) => {
+    const div = document.createElement('div');
+    weekContainer.appendChild(div);
+    div.classList.add('datewise-week');
+    div.innerHTML = week;
+});
