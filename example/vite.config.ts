002dd06ff8c231354ccd0af765317a3d9c336e92
Gagik-arch <gagik.chilingaryan2020@gmail.com>
2024-10-03 22:11:26 +0400

    public

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
+    publicDir: './public',
     server: {
         port: process.env.PORT as unknown as number,
     },
     plugins: [vercel()],
-    publicDir: 'public',
 });
