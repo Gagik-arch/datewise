a318dd082e8a3eb006a24a00ac537dd4cba82f53
Gagik-arch <gagik.chilingaryan2020@gmail.com>
2024-10-03 13:18:24 +0400

    calendar styles with animation

--- example/main.js
+++ example/main.js
@@ -8,22 +8,49 @@ const date = document.getElementById('datewise_date');
 
 console.log(calendar);
 
-calendar.weekDays.forEach((week) => {
+calendar.weekDays.forEach((week, i) => {
     const div = document.createElement('div');
     weekContainer.appendChild(div);
     div.classList.add('datewise_week');
+    div.dataset.weekday = i;
     div.innerHTML = week.slice(0, 3);
+
+    div.onmouseenter = (e) => {
+        const weekdays = [
+            ...document.querySelectorAll(
+                `[data-weekday='${e.target.dataset.weekday}']`
+            ),
+        ];
+        e.target.classList.add('datewise_week_hover');
+        weekdays.forEach((day) => {
+            day.classList.add('datewise_week_hover');
+        });
+        weekdays.at(-1).classList.add('datewise_week_hover_last');
+    };
+
+    div.onmouseleave = (e) => {
+        const weekdays = [
+            ...document.querySelectorAll(
+                `[data-weekday='${e.target.dataset.weekday}']`
+            ),
+        ];
+        weekdays.forEach((day) => {
+            day.classList.remove('datewise_week_hover');
+        });
+        weekdays.at(-1).classList.remove('datewise_week_hover_last');
+    };
 });
 
 const renderDays = () => {
     calendar.days.forEach((day) => {
-        const div = document.createElement('div');
-        dayContainer.appendChild(div);
-        div.classList.add('datewise_day');
-        if (day.status === 'selected-day') {
-            div.classList.add('datewise_selected_day');
-        }
-        div.innerHTML = day.label;
+        const parent = document.createElement('div');
+        const child = document.createElement('div');
+        dayContainer.appendChild(parent);
+        parent.appendChild(child);
+        parent.classList.add('datewise_day');
+        child.classList.add(`datewise_${day.status.replaceAll('-', '_')}`);
+        parent.dataset.weekday = day.date.getDay();
+        child.innerHTML = day.label;
     });
 };
 renderDays();
